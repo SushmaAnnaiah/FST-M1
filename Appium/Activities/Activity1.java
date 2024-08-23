67d4c06a1ae9cac5a722d184eca2507fe6a017ad
SushmaAnnaiah <152509796+SushmaAnnaiah@users.noreply.github.com>
2024-08-23 16:44:13 +0530

    Update Activity1.java

--- Appium/Activities/Activity1.java
+++ Appium/Activities/Activity1.java
@@ -1,71 +1,65 @@
-package project;
+package activities;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.time.Duration;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class Activity1 {
-	
+	//Declare driver
 	AppiumDriver driver;
-	WebDriverWait wait;
-
+	
 	@BeforeClass
-	public void setup() throws MalformedURLException, URISyntaxException {
-
-		// Desired Capabilities
-		UiAutomator2Options caps = new UiAutomator2Options().setPlatformName("andriod")
-				.setAutomationName("UiAutomator2").
-				setAppPackage("com.google.android.apps.tasks").
-				setAppActivity(".ui.TaskListsActivity").
+	public void setUP() throws MalformedURLException, URISyntaxException {
+	//Desired Capabilities
+		UiAutomator2Options caps = new UiAutomator2Options().
+				setPlatformName("android").
+				setAutomationName("UiAutomator2").
+				setAppPackage("com.google.android.calculator").
+				setAppActivity("com.android.calculator2.Calculator").
 				noReset();
 		
-		
-		//Set the Appium server URL
+		//Set Appium server url
 		URL serverURL = new URI("http://localhost:4723").toURL();
 		
 		//Initializing driver
 		driver = new AndroidDriver(serverURL, caps);
-		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 		
 	}
 	
 	@Test
-	public void taskTest() {
+	public void multiplicationTest() {
+		//Find digit 9 and press it
+		driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_9")).click();
 		
-		//Complete Activity with Google Tasks
-		driver.findElement(AppiumBy.accessibilityId("Create new task")).click();
-		wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.google.android.apps.tasks:id/add_task_title")));
-		 driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_title")).sendKeys("Adding the Google Task successfully");
-		 driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_done")).click();
-		 
-		 //Complete Activity with Google Keep
-		 driver.findElement(AppiumBy.accessibilityId("Create new task")).click();
-		 wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.google.android.apps.tasks:id/add_task_title")));
-		 driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_title")).sendKeys("Complete Activity with Google Keep");
-		 driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_done")).click();
-		 
-		 //Complete the second Activity Google Keep
-		 driver.findElement(AppiumBy.accessibilityId("Create new task")).click();
-		 wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("com.google.android.apps.tasks:id/add_task_title")));
-		 driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_title")).sendKeys("Complete the second Activity Google Keep");
-		 driver.findElement(AppiumBy.id("com.google.android.apps.tasks:id/add_task_done")).click();
+		//Find multiply and press it
+		driver.findElement(AppiumBy.accessibilityId("multiply")).click();
 		
-		}
-	
-	@AfterClass
-	public void teardown() {
-		driver.quit();
+		
+		
+		//Find digit 2 and press it
+		driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_2")).click();
+		
+		//Find digit 0 and press it
+		driver.findElement(AppiumBy.id("com.google.android.calculator:id/digit_0")).click();
+				
+		//Find equal and press it
+		driver.findElement(AppiumBy.accessibilityId("equals")).click();
+		
+		//Get the result and assert
+		String result = driver.findElement(AppiumBy.id("result_final")).getText();
+	    System.out.println("Sum of numbers: " + result);
+	    Assert.assertEquals(result, "180");
+		
 	}
-
 }
+
