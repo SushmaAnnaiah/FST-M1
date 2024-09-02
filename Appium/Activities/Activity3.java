273429900c885d8d4d2565fccbe7bfea0cddba9f
SushmaAnnaiah <152509796+SushmaAnnaiah@users.noreply.github.com>
2024-09-02 18:11:07 +0530

    Update Activity3.java

--- Appium/Activities/Activity3.java
+++ Appium/Activities/Activity3.java
@@ -13,7 +13,7 @@ import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
-public class Activity2 {
+public class Activity3 {
     // Driver Declaration
     AndroidDriver driver;
 
@@ -24,37 +24,84 @@ public class Activity2 {
         UiAutomator2Options options = new UiAutomator2Options();
         options.setPlatformName("android");
         options.setAutomationName("UiAutomator2");
-        options.setAppPackage("com.android.chrome");
-        options.setAppActivity("com.google.android.apps.chrome.Main");
+        options.setAppPackage("com.android.calculator2");
+        options.setAppActivity(".Calculator");
         options.noReset();
 
-        // Set the Appium server URL
+        // Server Address
         URL serverURL = new URI("http://localhost:4723").toURL();
 
         // Driver Initialization
         driver = new AndroidDriver(serverURL, options);
-
-        // Open the page in Chrome
-        driver.get("https://v1.training-support.net/");
     }
 
     // Test method
     @Test
-    public void chromeTest() {
-        // Find heading on the page
-        String pageHeading = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"Training Support\"]")).getText();
+    public void additionTest() {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.accessibilityId("plus")).click();
+        driver.findElement(AppiumBy.id("digit_9")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
 
-        // Print to console
-        System.out.println("Heading: " + pageHeading);
+        // Find the result
+        String result = driver.findElement(AppiumBy.id("result")).getText();
 
-        // Find and click the About Us link
-        driver.findElement(AppiumBy.accessibilityId("About Us")).click();
-
-        // Find heading of new page and print to console
-        String aboutPageHeading = driver.findElement(AppiumBy.xpath("//android.widget.TextView[@text=\"About Us\"]")).getText();
-        System.out.println(aboutPageHeading);
+        // Assertion
+        Assert.assertEquals(result, "14");
     }
 
+    // Test method
+    @Test
+    public void subtractTest() {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("digit_1")).click();
+        driver.findElement(AppiumBy.id("digit_0")).click();
+        driver.findElement(AppiumBy.accessibilityId("minus")).click();
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+        // Find the result
+        String result = driver.findElement(AppiumBy.id("result")).getText();
+
+        // Assertion
+        Assert.assertEquals(result, "5");
+    }
+
+    // Test method
+    @Test
+    public void multiplyTest() {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.accessibilityId("multiply")).click();
+        driver.findElement(AppiumBy.id("digit_1")).click();
+        driver.findElement(AppiumBy.id("digit_0")).click();
+        driver.findElement(AppiumBy.id("digit_0")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+        // Find the result
+        String result = driver.findElement(AppiumBy.id("result")).getText();
+
+        // Assertion
+        Assert.assertEquals(result, "500");
+    }
+
+    // Test method
+    @Test
+    public void divideTest() {
+        // Perform the calculation
+        driver.findElement(AppiumBy.id("digit_5")).click();
+        driver.findElement(AppiumBy.id("digit_0")).click();
+        driver.findElement(AppiumBy.accessibilityId("divide")).click();
+        driver.findElement(AppiumBy.id("digit_2")).click();
+        driver.findElement(AppiumBy.accessibilityId("equals")).click();
+
+        // Find the result
+        String result = driver.findElement(AppiumBy.id("result")).getText();
+
+        // Assertion
+        Assert.assertEquals(result, "25");
+    }
 
     // Tear down method
     @AfterClass
